f49d89f14a53a3ff8d6df6f3aa3a0ac1c1127964
Zhao KK <kikakkz@hotmail.com>
2023-01-05 02:17:30 +0000

    Support create coupon

---
+++ pkg/coupon/coupon/create.go
@@ -0,0 +1,11 @@
+package coupon
+
+import (
+	"context"
+
+	couponmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon/coupon"
+)
+
+func CreateCoupon(ctx context.Context, in *couponmwpb.CouponReq) (*couponmwpb.Coupon, error) {
+	return nil, nil
+}
