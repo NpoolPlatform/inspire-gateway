3277128602a167b648ef57e447252d14001cb4dc
JobNing <q178149@163.com>
2023-02-17 17:33:42 +0800

    add Migrate

--- pkg/migrator/migrator.go
+++ pkg/migrator/migrator.go
@@ -5,6 +5,9 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"github.com/NpoolPlatform/inspire-manager/pkg/db"
+	"github.com/NpoolPlatform/inspire-manager/pkg/db/ent"
+	"github.com/shopspring/decimal"
 	"time"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/config"
@@ -65,5 +68,66 @@ func open(hostname string) (conn *sql.DB, err error) {
 }
 
 func Migrate(ctx context.Context) error {
-	return nil
+	var err error
+
+	if err := db.Init(); err != nil {
+		return err
+	}
+	logger.Sugar().Infow("Migrate order", "Start", "...")
+	defer func() {
+		logger.Sugar().Infow("Migrate order", "Done", "...", "error", err)
+	}()
+
+	return db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		infos, err := cli.
+			ArchivementDetail.
+			Query().
+			All(_ctx)
+		if err != nil {
+			return err
+		}
+
+		for _, info := range infos {
+			if info.Units == 0 {
+				continue
+			}
+			units := decimal.NewFromInt32(int32(info.Units))
+			_, err := cli.
+				ArchivementDetail.
+				UpdateOneID(info.ID).
+				SetUnitsV1(units).
+				Save(_ctx)
+			if err != nil {
+				return err
+			}
+		}
+		infos1, err := cli.
+			ArchivementGeneral.
+			Query().
+			All(_ctx)
+		if err != nil {
+			return err
+		}
+
+		for _, info := range infos1 {
+			u := cli.
+				ArchivementGeneral.
+				UpdateOneID(info.ID)
+
+			if info.TotalUnits != 0 {
+				totalUnits := decimal.NewFromInt32(int32(info.TotalUnits))
+				u.SetTotalUnitsV1(totalUnits)
+			}
+
+			if info.SelfUnits != 0 {
+				selfUnits := decimal.NewFromInt32(int32(info.SelfUnits))
+				u.SetSelfUnitsV1(selfUnits)
+			}
+			_, err = u.Save(_ctx)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
 }
