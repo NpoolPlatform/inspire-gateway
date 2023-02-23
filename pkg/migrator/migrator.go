701b3daa5c3d3e16c9f18eaa2b98862d0be37e23
JobNing <q178149@163.com>
2023-02-23 18:20:33 +0800

    update migration

--- pkg/migrator/migrator.go
+++ pkg/migrator/migrator.go
@@ -5,8 +5,11 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"github.com/NpoolPlatform/go-service-framework/pkg/redis"
 	"github.com/NpoolPlatform/inspire-manager/pkg/db"
 	"github.com/NpoolPlatform/inspire-manager/pkg/db/ent"
+	archivementdetailent "github.com/NpoolPlatform/inspire-manager/pkg/db/ent/archivementdetail"
+	archivementgeneralent "github.com/NpoolPlatform/inspire-manager/pkg/db/ent/archivementgeneral"
 	"github.com/shopspring/decimal"
 	"time"
 
@@ -14,16 +17,23 @@ import (
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 
 	constant "github.com/NpoolPlatform/go-service-framework/pkg/mysql/const"
+	constant1 "github.com/NpoolPlatform/inspire-gateway/pkg/message/const"
 )
 
 const (
-	keyUsername = "username"
-	keyPassword = "password"
-	keyDBName   = "database_name"
-	maxOpen     = 10
-	maxIdle     = 10
-	MaxLife     = 3
+	keyUsername  = "username"
+	keyPassword  = "password"
+	keyDBName    = "database_name"
+	maxOpen      = 10
+	maxIdle      = 10
+	MaxLife      = 3
+	keyServiceID = "serviceid"
 )
+
+func lockKey() string {
+	serviceID := config.GetStringValueWithNameSpace(constant1.ServiceName, keyServiceID)
+	return fmt.Sprintf("migrator:%v", serviceID)
+}
 
 func dsn(hostname string) (string, error) {
 	username := config.GetStringValueWithNameSpace(constant.MysqlServiceName, keyUsername)
@@ -75,24 +85,35 @@ func Migrate(ctx context.Context) error {
 	}
 	logger.Sugar().Infow("Migrate order", "Start", "...")
 	defer func() {
+		_ = redis.Unlock(lockKey())
 		logger.Sugar().Infow("Migrate order", "Done", "...", "error", err)
 	}()
 
-	return db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		infos, err := cli.
+	err = redis.TryLock(lockKey(), 0)
+	if err != nil {
+		return err
+	}
+
+	return db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
+		infos, err := tx.
 			ArchivementDetail.
 			Query().
+			Select(
+				archivementdetailent.FieldID,
+				archivementdetailent.FieldUnits,
+			).
 			All(_ctx)
 		if err != nil {
 			return err
 		}
 
 		for _, info := range infos {
-			if info.Units == 0 {
-				continue
+			units := decimal.NewFromInt(0)
+			if info.Units != 0 {
+				units = decimal.NewFromInt32(int32(info.Units))
 			}
-			units := decimal.NewFromInt32(int32(info.Units))
-			_, err := cli.
+
+			_, err = tx.
 				ArchivementDetail.
 				UpdateOneID(info.ID).
 				SetUnitsV1(units).
@@ -101,28 +122,35 @@ func Migrate(ctx context.Context) error {
 				return err
 			}
 		}
-		infos1, err := cli.
+		infos1, err := tx.
 			ArchivementGeneral.
 			Query().
+			Select(
+				archivementgeneralent.FieldID,
+				archivementgeneralent.FieldTotalUnits,
+				archivementgeneralent.FieldSelfUnits,
+			).
 			All(_ctx)
 		if err != nil {
 			return err
 		}
 
 		for _, info := range infos1 {
-			u := cli.
+			u := tx.
 				ArchivementGeneral.
 				UpdateOneID(info.ID)
 
+			totalUnits := decimal.NewFromInt(0)
 			if info.TotalUnits != 0 {
-				totalUnits := decimal.NewFromInt32(int32(info.TotalUnits))
-				u.SetTotalUnitsV1(totalUnits)
+				totalUnits = decimal.NewFromInt32(int32(info.TotalUnits))
 			}
+			u.SetTotalUnitsV1(totalUnits)
 
+			selfUnits := decimal.NewFromInt(0)
 			if info.SelfUnits != 0 {
-				selfUnits := decimal.NewFromInt32(int32(info.SelfUnits))
-				u.SetSelfUnitsV1(selfUnits)
+				selfUnits = decimal.NewFromInt32(int32(info.SelfUnits))
 			}
+			u.SetSelfUnitsV1(selfUnits)
 			_, err = u.Save(_ctx)
 			if err != nil {
 				return err
