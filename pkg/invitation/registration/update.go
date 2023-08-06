0cb0488b63a2551f4fd9c60025157295d3f0746e
Zhao KK <kikakkz@hotmail.com>
2023-08-06 06:10:45 +0000

    Fix: http://47.116.78.131:10083/issues/1811

--- pkg/invitation/registration/update.go
+++ pkg/invitation/registration/update.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	usermwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/user"
 	regmwcli "github.com/NpoolPlatform/inspire-middleware/pkg/client/invitation/registration"
 	npool "github.com/NpoolPlatform/message/npool/inspire/gw/v1/invitation/registration"
 	regmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/invitation/registration"
@@ -13,7 +14,27 @@ func (h *Handler) UpdateRegistration(ctx context.Context) (*npool.Registration, 
 	if h.ID == nil {
 		return nil, fmt.Errorf("invalid id")
 	}
-	_, err := regmwcli.UpdateRegistration(ctx, &regmwpb.RegistrationReq{
+	if h.InviterID == nil {
+		return nil, fmt.Errorf("invalid inviterid")
+	}
+
+	info, err := h.GetRegistration(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if info.InviterID == *h.InviterID || info.InviteeID == *h.InviterID {
+		return nil, fmt.Errorf("invalid inviterid")
+	}
+
+	exist, err := usermwcli.ExistUser(ctx, info.AppID, *h.InviterID)
+	if err != nil {
+		return nil, err
+	}
+	if !exist {
+		return nil, fmt.Errorf("invalid inviterid")
+	}
+
+	_, err = regmwcli.UpdateRegistration(ctx, &regmwpb.RegistrationReq{
 		ID:        h.ID,
 		InviterID: h.InviterID,
 	})
