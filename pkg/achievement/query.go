19e4db6082913f99f1eb57dca7dbdf0f5679e669
Zhao KK <kikakkz@hotmail.com>
2023-08-10 08:36:33 +0000

    update get achievement

--- pkg/achievement/query.go
+++ pkg/achievement/query.go
@@ -65,7 +65,7 @@ func (h *queryHandler) getInvitees(ctx context.Context) error {
 		h.registrations[registration.InviteeID] = registration
 		h.inviteIDs = append(h.inviteIDs, registration.InviteeID)
 	}
-	h.total = total
+	h.total = total + len(h.inviteIDs)
 	return nil
 }
 
