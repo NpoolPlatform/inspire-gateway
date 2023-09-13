7cbc6fcc6efe11cf21c5e9fd2dca1dfa159015b2
Zhao KK <kikakkz@hotmail.com>
2023-09-13 04:16:44 +0000

    Add migration

--- pkg/migrator/migrator.go
+++ pkg/migrator/migrator.go
@@ -15,7 +15,9 @@ import (
 	servicename "github.com/NpoolPlatform/inspire-gateway/pkg/servicename"
 	"github.com/NpoolPlatform/inspire-middleware/pkg/db"
 	"github.com/NpoolPlatform/inspire-middleware/pkg/db/ent"
+	entachievement "github.com/NpoolPlatform/inspire-middleware/pkg/db/ent/achievement"
 	entcommission "github.com/NpoolPlatform/inspire-middleware/pkg/db/ent/commission"
+	entstatement "github.com/NpoolPlatform/inspire-middleware/pkg/db/ent/statement"
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 	appgoodmwpb "github.com/NpoolPlatform/message/npool/good/mw/v1/app/good"
@@ -80,6 +82,138 @@ func open(hostname string) (conn *sql.DB, err error) {
 	return conn, nil
 }
 
+func migrateCommission(ctx context.Context, tx *ent.Tx) error {
+	commissions, err := tx.
+		Commission.
+		Query().
+		Where(
+			entcommission.DeletedAt(0),
+		).
+		All(ctx)
+	if err != nil {
+		return err
+	}
+
+	goods := map[uuid.UUID]*appgoodmwpb.Good{}
+	for _, commission := range commissions {
+		if commission.AppGoodID != uuid.Nil {
+			continue
+		}
+		good, ok := goods[commission.GoodID]
+		if !ok {
+			good, err = appgoodmwcli.GetGoodOnly(ctx, &appgoodmwpb.Conds{
+				AppID:  &basetypes.StringVal{Op: cruder.EQ, Value: commission.AppID.String()},
+				GoodID: &basetypes.StringVal{Op: cruder.EQ, Value: commission.GoodID.String()},
+			})
+			if err != nil {
+				continue
+			}
+			if good == nil {
+				continue
+			}
+			goods[commission.GoodID] = good
+		}
+
+		id := uuid.MustParse(good.ID)
+		if _, err := tx.
+			Commission.
+			UpdateOneID(commission.ID).
+			SetAppGoodID(id).
+			Save(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func migrateAchievement(ctx context.Context, tx *ent.Tx) error {
+	achievements, err := tx.
+		Achievement.
+		Query().
+		Where(
+			entachievement.DeletedAt(0),
+		).
+		All(ctx)
+	if err != nil {
+		return err
+	}
+
+	goods := map[uuid.UUID]*appgoodmwpb.Good{}
+	for _, achievement := range achievements {
+		if achievement.AppGoodID != uuid.Nil {
+			continue
+		}
+		good, ok := goods[achievement.GoodID]
+		if !ok {
+			good, err = appgoodmwcli.GetGoodOnly(ctx, &appgoodmwpb.Conds{
+				AppID:  &basetypes.StringVal{Op: cruder.EQ, Value: achievement.AppID.String()},
+				GoodID: &basetypes.StringVal{Op: cruder.EQ, Value: achievement.GoodID.String()},
+			})
+			if err != nil {
+				continue
+			}
+			if good == nil {
+				continue
+			}
+			goods[achievement.GoodID] = good
+		}
+
+		id := uuid.MustParse(good.ID)
+		if _, err := tx.
+			Achievement.
+			UpdateOneID(achievement.ID).
+			SetAppGoodID(id).
+			Save(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func migrateStatement(ctx context.Context, tx *ent.Tx) error {
+	statements, err := tx.
+		Statement.
+		Query().
+		Where(
+			entstatement.DeletedAt(0),
+		).
+		All(ctx)
+	if err != nil {
+		return err
+	}
+
+	goods := map[uuid.UUID]*appgoodmwpb.Good{}
+	for _, statement := range statements {
+		if statement.AppGoodID != uuid.Nil {
+			continue
+		}
+		good, ok := goods[statement.GoodID]
+		if !ok {
+			good, err = appgoodmwcli.GetGoodOnly(ctx, &appgoodmwpb.Conds{
+				AppID:  &basetypes.StringVal{Op: cruder.EQ, Value: statement.AppID.String()},
+				GoodID: &basetypes.StringVal{Op: cruder.EQ, Value: statement.GoodID.String()},
+			})
+			if err != nil {
+				continue
+			}
+			if good == nil {
+				continue
+			}
+			goods[statement.GoodID] = good
+		}
+
+		id := uuid.MustParse(good.ID)
+		if _, err := tx.
+			Statement.
+			UpdateOneID(statement.ID).
+			SetAppGoodID(id).
+			Save(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func Migrate(ctx context.Context) error {
 	if err := redis2.TryLock(lockKey(), 0); err != nil {
 		return err
@@ -89,45 +223,14 @@ func Migrate(ctx context.Context) error {
 	}()
 
 	return db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
-		commissions, err := tx.
-			Commission.
-			Query().
-			Where(
-				entcommission.DeletedAt(0),
-			).
-			All(_ctx)
-		if err != nil {
+		if err := migrateCommission(_ctx, tx); err != nil {
 			return err
 		}
-
-		goods := map[uuid.UUID]*appgoodmwpb.Good{}
-		for _, commission := range commissions {
-			if commission.AppGoodID != uuid.Nil {
-				continue
-			}
-			good, ok := goods[commission.GoodID]
-			if !ok {
-				good, err = appgoodmwcli.GetGoodOnly(ctx, &appgoodmwpb.Conds{
-					AppID:  &basetypes.StringVal{Op: cruder.EQ, Value: commission.AppID.String()},
-					GoodID: &basetypes.StringVal{Op: cruder.EQ, Value: commission.GoodID.String()},
-				})
-				if err != nil {
-					continue
-				}
-				if good == nil {
-					continue
-				}
-				goods[commission.GoodID] = good
-			}
-
-			id := uuid.MustParse(good.ID)
-			if _, err := tx.
-				Commission.
-				UpdateOneID(commission.ID).
-				SetAppGoodID(id).
-				Save(_ctx); err != nil {
-				return err
-			}
+		if err := migrateAchievement(_ctx, tx); err != nil {
+			return err
+		}
+		if err := migrateStatement(_ctx, tx); err != nil {
+			return err
 		}
 		return nil
 	})
