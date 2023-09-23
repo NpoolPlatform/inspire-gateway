26825c25237f1f93b233a3ebdd7b40b76e2157f3
Zhao KK <kikakkz@hotmail.com>
2023-09-23 12:43:43 +0000

    Format code

--- pkg/pubsub/subscriber.go
+++ pkg/pubsub/subscriber.go
@@ -129,31 +129,14 @@ func stat(ctx context.Context, mid string, uid uuid.UUID, rid *uuid.UUID) (bool,
 // Process will consume the message and return consuming state
 //  Return
 //   error   reason of error, if nil, means the message should be acked
-func process(ctx context.Context, mid string, uid uuid.UUID, req interface{}) (err error) {
-	defer func() {
-		if err != nil {
-			logger.Sugar().Warnw(
-				"process",
-				"MID", mid,
-				"UID", uid,
-				"Req", req,
-				"Error", err,
-			)
-		}
-	}()
-
+func process(ctx context.Context, uid uuid.UUID, req interface{}) (err error) {
 	switch mid {
 	case basetypes.MsgID_CreateCommissionReq.String():
 		err = commission.Apply(ctx, req)
 	default:
 		return nil
 	}
-
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
 
 // No matter what handler return, the message will be acked, unless handler halt
