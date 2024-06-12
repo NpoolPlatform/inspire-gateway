cd359fd01efa6a665004f649d7989d6a29dfa066
lidaqiang <dqli161@163.com>
2024-06-12 15:13:54 +0800

    support migrate

--- go.mod
+++ go.mod
@@ -9,10 +9,10 @@ require (
 	github.com/NpoolPlatform/g11n-middleware v0.0.0-20231026021135-ec3cd368fc24
 	github.com/NpoolPlatform/go-service-framework v0.0.0-20240510075442-89278cb5cf47
 	github.com/NpoolPlatform/good-middleware v0.0.0-20240525100138-23fec8e8e6a0
-	github.com/NpoolPlatform/inspire-middleware v0.0.0-20240523075812-7d427e466af3
+	github.com/NpoolPlatform/inspire-middleware v0.0.0-20240611093952-39440da82554
 	github.com/NpoolPlatform/ledger-middleware v0.0.0-20240304030403-1e129dfe4e3f
 	github.com/NpoolPlatform/libent-cruder v0.0.0-20240514082633-598d5fc7b1e3
-	github.com/NpoolPlatform/message v0.0.0-20240527093630-5fdacaf0ea08
+	github.com/NpoolPlatform/message v0.0.0-20240528041351-d3a8581059dd
 	github.com/NpoolPlatform/notif-middleware v0.0.0-20231110071848-7e72bb63090a
 	github.com/NpoolPlatform/order-middleware v0.0.0-20240528085444-96f485ad0ea3
 	github.com/NpoolPlatform/third-middleware v0.0.0-20231011073243-59e4e2a0a8ac
--- pkg/migrator/migrator.go
+++ pkg/migrator/migrator.go
@@ -13,6 +13,7 @@ import (
 	servicename "github.com/NpoolPlatform/inspire-gateway/pkg/servicename"
 	"github.com/NpoolPlatform/inspire-middleware/pkg/db"
 	"github.com/NpoolPlatform/inspire-middleware/pkg/db/ent"
+	entachievement "github.com/NpoolPlatform/inspire-middleware/pkg/db/ent/achievement"
 	entstatement "github.com/NpoolPlatform/inspire-middleware/pkg/db/ent/statement"
 	inspiretypes "github.com/NpoolPlatform/message/npool/basetypes/inspire/v1"
 	ordertypes "github.com/NpoolPlatform/message/npool/basetypes/order/v1"
@@ -302,6 +303,107 @@ func migrateStatement(ctx context.Context, tx *ent.Tx) error {
 	return nil
 }
 
+func migrateAchievement(ctx context.Context, tx *ent.Tx) error {
+	achievements, err := tx.
+		Achievement.
+		Query().
+		Where(
+			entachievement.DeletedAt(0),
+		).
+		All(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, achievement := range achievements {
+		if _, err := tx.
+			GoodAchievement.
+			Create().
+			SetAppID(achievement.AppID).
+			SetUserID(achievement.UserID).
+			SetGoodID(achievement.GoodID).
+			SetAppGoodID(achievement.AppGoodID).
+			SetTotalUnits(achievement.TotalUnitsV1).
+			SetSelfUnits(achievement.SelfUnitsV1).
+			SetTotalAmountUsd(achievement.TotalAmount).
+			SetSelfAmountUsd(achievement.SelfAmount).
+			SetTotalCommissionUsd(achievement.TotalCommission).
+			SetSelfCommissionUsd(achievement.SelfCommission).
+			Save(ctx); err != nil {
+			return err
+		}
+		if _, err := tx.
+			GoodCoinAchievement.
+			Create().
+			SetAppID(achievement.AppID).
+			SetUserID(achievement.UserID).
+			SetGoodCoinTypeID(achievement.CoinTypeID).
+			SetTotalUnits(achievement.TotalUnitsV1).
+			SetSelfUnits(achievement.SelfUnitsV1).
+			SetTotalAmountUsd(achievement.TotalAmount).
+			SetSelfAmountUsd(achievement.SelfAmount).
+			SetTotalCommissionUsd(achievement.TotalCommission).
+			SetSelfCommissionUsd(achievement.SelfCommission).
+			Save(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func migrateAchievementStatement(ctx context.Context, tx *ent.Tx) error {
+	statements, err := tx.
+		Statement.
+		Query().
+		Where(
+			entstatement.DeletedAt(0),
+		).
+		All(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, statement := range statements {
+		if statement.PaymentCoinUsdCurrency.Cmp(decimal.NewFromInt(0)) <= 0 {
+			logger.Sugar().Warn("invalid payment coin usd currency: %v", statement.PaymentCoinUsdCurrency.String())
+			continue
+		}
+		paymentAmountUSD := statement.PaymentCoinUsdCurrency.Mul(statement.Amount)
+
+		if _, err := tx.
+			OrderStatement.
+			Create().
+			SetEntID(statement.EntID).
+			SetAppID(statement.AppID).
+			SetUserID(statement.UserID).
+			SetAppGoodID(statement.AppGoodID).
+			SetOrderID(statement.OrderID).
+			SetOrderUserID(statement.DirectContributorID).
+			SetGoodCoinTypeID(statement.CoinTypeID).
+			SetUnits(statement.UnitsV1).
+			SetGoodValueUsd(statement.UsdAmount).
+			SetPaymentAmountUsd(paymentAmountUSD).
+			SetCommissionAmountUsd(statement.Commission).
+			SetAppConfigID(statement.AppConfigID).
+			SetCommissionConfigID(statement.CommissionConfigID).
+			SetCommissionConfigType(statement.CommissionConfigType).
+			Save(ctx); err != nil {
+			return err
+		}
+		if _, err := tx.
+			OrderPaymentStatement.
+			Create().
+			SetStatementID(statement.EntID).
+			SetPaymentCoinTypeID(statement.PaymentCoinTypeID).
+			SetAmount(statement.Amount).
+			SetCommissionAmount(statement.Commission).
+			Save(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func migrateAchievementUser(ctx context.Context, tx *ent.Tx) error {
 	var err error
 	r, err := tx.QueryContext(ctx, "select id,app_id,ent_id,deleted_at from appuser_manager.app_users where deleted_at=0")
@@ -474,6 +576,14 @@ func Migrate(ctx context.Context) error {
 			return err
 		}
 		if err := migrateStatement(_ctx, tx); err != nil {
+			logger.Sugar().Errorw("Migrate", "error", err)
+			return err
+		}
+		if err := migrateAchievementStatement(_ctx, tx); err != nil {
+			logger.Sugar().Errorw("Migrate", "error", err)
+			return err
+		}
+		if err := migrateAchievement(_ctx, tx); err != nil {
 			logger.Sugar().Errorw("Migrate", "error", err)
 			return err
 		}
